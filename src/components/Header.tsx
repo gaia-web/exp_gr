335ad2453262c19637a557ba33c5f88c9d427dec
Sean Wong <seanwong24@live.com>
2025-06-21 23:28:49 +0000

    clean up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
-import { useLocation } from 'preact-iso';
+import { useLocation } from "preact-iso";
 
 export function Header() {
-	const { url } = useLocation();
+  const { url } = useLocation();
 
-	return (
-		<header>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
-			</nav>
-		</header>
-	);
+  return (
+    <header class="neumo hollow">
+      <nav>
+        <a href="/" class={`neumo ${url === "/" ? "active hollow" : ""}`}>
+          Home
+        </a>
+        <a href="/404" class={`neumo ${url === "/404" ? "active hollow" : ""}`}>
+          404
+        </a>
+      </nav>
+    </header>
+  );
 }
