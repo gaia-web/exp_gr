e0c966e5fd8444a01b128de9f4c19ebbd0d45231
Sean Wong <seanwong24@live.com>
2025-06-24 12:48:52 +0000

    add a simple view transition demo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,57 @@
-import './style.css';
+import { useSignal, useSignalEffect } from "@preact/signals";
+import "./style.css";
+import { useRef } from "preact/hooks";
 
 export function Home() {
-	return (
-		<div class="home">
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores at, doloremque officiis maxime vitae recusandae. Libero et eligendi itaque id quia autem, animi ipsa ad architecto, nemo officia soluta officiis.</p>
-		</div>
-	);
+  const dialogRef = useRef<HTMLDialogElement>();
+  const foo = useSignal(false);
+
+  useSignalEffect(() => {
+    const shouldOpen = foo.value;
+    const callback = () => {
+      if (!dialogRef.current) return;
+      if (shouldOpen) {
+        dialogRef.current.style.viewTransitionName = "dialog";
+        dialogRef.current.showModal();
+        return;
+      }
+      dialogRef.current.style.viewTransitionName = "dialog";
+      dialogRef.current?.close();
+    };
+    if (document.startViewTransition) {
+      document.startViewTransition(callback);
+    } else {
+      callback();
+    }
+  });
+
+  return (
+    <div class="home">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores at,
+        doloremque officiis maxime vitae recusandae. Libero et eligendi itaque
+        id quia autem, animi ipsa ad architecto, nemo officia soluta officiis.
+      </p>
+      <button
+        class="neumo"
+        onClick={() => {
+          foo.value = true;
+        }}
+      >
+        Open Modal
+      </button>
+      <dialog ref={dialogRef} style="background: var(--background-color);">
+        Foo
+        <br />
+        <button
+          class="neumo"
+          onClick={() => {
+            foo.value = false;
+          }}
+        >
+          Close Modal
+        </button>
+      </dialog>
+    </div>
+  );
 }
