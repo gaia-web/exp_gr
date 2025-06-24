4654aa287167991701dc1c4a9f9dd53dbb1133e7
Sean Wong <seanwong24@live.com>
2025-06-24 12:08:30 +0000

    update peer test page

--- src/pages/PeerTest/index.tsx
+++ src/pages/PeerTest/index.tsx
@@ -1,46 +1,28 @@
-import { useComputed, useSignal, useSignalEffect } from "@preact/signals";
-import { DataConnection, Peer, PeerJSOption } from "peerjs";
-
-const PEER_ID_PREFIX = "1uX68Fu0mzVKNp5h";
-const PEER_JS_OPTIONS: PeerJSOption = { debug: 3 };
+import { Peer } from "peerjs";
+import {
+  peer,
+  PEER_ID_PREFIX,
+  PEER_JS_OPTIONS,
+  playerName,
+  roomName,
+} from "../../utils/peer";
 
 export function PeerTest() {
-  const peer = useSignal<Peer>();
-  const connection = useSignal<DataConnection>();
-  const roomName = useSignal("");
-  const trimmedRoomName = useComputed(() => roomName.value.trim());
-
-  useSignalEffect(() => {
-    const p = peer.value;
-    if (!p) return;
-    p.on("connection", (c) => {
-      connection.value = c;
-    });
-  });
-
-  useSignalEffect(() => {
-    const c = connection.value;
-    if (!c) return;
-    c.on("open", () => {
-      c.send("hi!");
-    }).on("data", (d) => {
-      alert(`Received:\n${d}`);
-      const content = prompt(
-        "Type something to send back (leave blank to break the loop)",
-        ""
-      );
-      if (content) {
-        c.send(content);
-      }
-    });
-  });
-
   return (
     <div class="peer-test">
       <input
         class="neumo"
         placeholder="Enter room name here"
-        onChange={({ currentTarget }) => (roomName.value = currentTarget.value)}
+        onChange={({ currentTarget }) =>
+          (roomName.value = currentTarget.value?.trim())
+        }
+      />
+      <input
+        class="neumo"
+        placeholder="Enter player name here"
+        onChange={({ currentTarget }) =>
+          (playerName.value = currentTarget.value)
+        }
       />
       <br />
       <button class="neumo" onClick={createRoom}>
@@ -50,39 +32,38 @@ export function PeerTest() {
       <button class="neumo" onClick={joinRoom}>
         Join Room
       </button>
+      <br />
+      <b>Open console to see the output</b>
     </div>
   );
 
   function createRoom() {
-    if (!roomName) {
+    if (!roomName.value) {
       alert("A room name is required.");
       return;
     }
-    peer.value?.destroy();
-    peer.value = new Peer(
-      `${PEER_ID_PREFIX}_${trimmedRoomName}`,
-      PEER_JS_OPTIONS
-    );
-    if (peer.value) {
-      alert("Room created, waiting for connection.");
+    if (!playerName.value) {
+      alert("A player name is required.");
+      return;
     }
+    peer.value?.destroy();
+    peer.value = new Peer(`${PEER_ID_PREFIX}_${roomName}`, PEER_JS_OPTIONS);
+    console.log(
+      `Create a room with name ${roomName} as ${playerName} and wait for players to join.`
+    );
   }
 
   function joinRoom() {
-    if (!roomName) {
+    if (!roomName.value) {
       alert("A room name is required.");
+      return;
+    }
+    if (!playerName.value) {
+      alert("A player name is required.");
       return;
     }
     peer.value?.destroy();
     peer.value = new Peer(PEER_JS_OPTIONS);
-    if (!peer.value) return;
-    peer.value.on("open", () => {
-      connection.value = peer.value.connect(
-        `${PEER_ID_PREFIX}_${trimmedRoomName}`
-      );
-      if (connection.value) {
-        alert("Connected, sending hi~~");
-      }
-    });
+    console.log(`Join room ${roomName} as ${playerName}.`);
   }
 }
---
+++ src/utils/peer.ts
@@ -0,0 +1,100 @@
+import { computed, effect, signal } from "@preact/signals";
+import Peer, { DataConnection, PeerJSOption } from "peerjs";
+
+export const PEER_ID_PREFIX = "1uX68Fu0mzVKNp5h";
+export const PEER_JS_OPTIONS: PeerJSOption = { debug: 0 };
+
+enum DataType {
+  UPDATE_PLAYER_NAME = "update_player_name",
+  UPDATE_PLAYER_LIST = "update_player_list",
+}
+
+export const peer = signal<Peer>();
+export const isHostPeer = computed(() =>
+  peer.value.id.startsWith(PEER_ID_PREFIX)
+);
+export const roomName = signal<string>();
+export const playerName = signal<string>();
+export const connectionMap = signal<Map<DataConnection, string>>(new Map());
+export const dataHandler = signal<
+  (data: { type?: string; value?: unknown }, connection: DataConnection) => void
+>((data, connection) => {
+  switch (data.type) {
+    case DataType.UPDATE_PLAYER_NAME:
+      if (!data) break;
+      const map = new Map(connectionMap.value);
+      map.set(connection, data.value.toString());
+      connectionMap.value = map;
+      console.log(`Peer ${connection.peer} updated its name as ${data.value}`);
+      break;
+    case DataType.UPDATE_PLAYER_LIST:
+      console.log(`Player list updated as: `, data.value);
+      break;
+  }
+});
+
+function updateDataHandler(c: DataConnection) {
+  c.off("data").on("data", (data) => dataHandler.value?.(data, c));
+}
+
+function sendPlayerName(c: DataConnection) {
+  c.send({ type: DataType.UPDATE_PLAYER_NAME, value: playerName.value });
+}
+
+function notifyPlayerListUpdate() {
+  const connectionAndPlayerNamePairs = [...connectionMap.value.entries()];
+  for (const [c] of connectionAndPlayerNamePairs) {
+    c.send({
+      type: DataType.UPDATE_PLAYER_LIST,
+      value: [
+        { id: peer.value.id, playerName: playerName.value },
+        ...connectionAndPlayerNamePairs.map(([p, n]) => ({
+          id: p.peer,
+          playerName: n,
+        })),
+      ],
+    });
+  }
+}
+
+effect(() => {
+  const p = peer.value;
+  if (!p) return;
+  p.off("error")
+    .on("error", (e) => {
+      if (e.type === "unavailable-id") {
+        alert(
+          "Room with this name has already been created. You may want to join instead."
+        );
+      }
+    })
+    .off("connection")
+    .on("connection", (c) => {
+      updateDataHandler(c);
+      const map = new Map(connectionMap.value);
+      map.set(c, c.connectionId);
+      connectionMap.value = map;
+      c.off("open").on("open", () => {
+        console.log(`New player ${c.peer} joined.`);
+        sendPlayerName(c);
+        notifyPlayerListUpdate();
+      });
+    });
+  p.off("open").on("open", () => {
+    if (!isHostPeer.value) {
+      const connection = p.connect(`${PEER_ID_PREFIX}_${roomName}`);
+      connection.on("open", () => {
+        sendPlayerName(connection);
+      });
+      const map = new Map(connectionMap.value);
+      map.set(connection, connection.connectionId);
+      connectionMap.value = map;
+    }
+  });
+});
+
+effect(() => {
+  for (const [c] of connectionMap.value) {
+    updateDataHandler(c);
+  }
+});
