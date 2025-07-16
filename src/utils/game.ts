b98a8589fc992223e8fa0cb54059485536f72473
Sean Wong <seanwong24@live.com>
2025-07-16 03:12:23 +0000

    define some types and helpers

---
+++ src/utils/game.ts
@@ -0,0 +1,47 @@
+export enum GameStatus {
+  READY = "ready",
+  RETIRED = "retired",
+}
+
+export enum GameStateMessageType {
+  PLAYER_INFO = "player_info",
+  PLAYER_LIST = "player_list",
+}
+
+type PlayerID = string;
+type PlayerName = string;
+
+export type GameListMessage = {
+  id: string;
+  label: string;
+  description: string;
+  pluginUrl: string;
+}[];
+
+export type GameStatusMessage =
+  | {
+      type: GameStatus.READY;
+      value: string;
+    }
+  | { type: GameStatus.RETIRED };
+
+export type GameStateMessage = { to?: string } & (
+  | {
+      type: string;
+      value: unknown;
+    }
+  | {
+      type: GameStateMessageType.PLAYER_INFO;
+      value: {
+        id: PlayerID;
+        name: PlayerName;
+        isHost: boolean;
+      };
+    }
+  | {
+      type: GameStateMessageType.PLAYER_LIST;
+      value: [PlayerID, PlayerName][];
+    }
+);
+
+export function sendMessageToGamePlugin() {}
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -2,12 +2,16 @@ import { DataConnection } from "peerjs";
 import { ChatMessage, insertChatMessageIntoHistory } from "./chat";
 import { connectionMap, isHost } from "./peer";
 import { exitRoom, playerMap } from "./session";
+import { GameListMessage, GameStatus, GameStatusMessage } from "./game";
 
 export enum MessageType {
   PLAYER_NAME = "player_name",
   UNAVAILABLE_PLAYER_NAME = "unavailable_player_name",
   PLAYER_LIST = "player_list",
   CHAT_MESSAGE = "chat_message",
+  GAME_LIST = "game_list",
+  GAME_STATUS = "game_status",
+  GAME_STATE = "game_state",
 }
 
 export type Message<T = unknown> = {
@@ -23,6 +27,9 @@ export const messageHandlerDict: Record<
   [MessageType.UNAVAILABLE_PLAYER_NAME]: handleUnavailablePlayerNameMessage,
   [MessageType.PLAYER_LIST]: handlePlayerListMessage,
   [MessageType.CHAT_MESSAGE]: handleChatMessage,
+  [MessageType.GAME_LIST]: handleGameListMessage,
+  [MessageType.GAME_STATUS]: handleGameStatusMessage,
+  [MessageType.GAME_STATE]: handleGameStateMessage,
 };
 
 // TODO instead of letting client disconnect from Host, we should let host disconnect client
@@ -94,12 +101,39 @@ function handleChatMessage(message: Message<ChatMessage>) {
   insertChatMessageIntoHistory(chatMessage);
 }
 
+function handleGameStatusMessage(message: Message<GameStatusMessage>) {
+  if (message.type !== MessageType.GAME_STATUS) throw "Wrong message type";
+  if (isHost.value) return;
+  switch (message.value?.type) {
+    case GameStatus.READY:
+      // TODO loads the game and maybe also navigate to the playing page
+      console.info(`The host started a game with id ${message.value.value}.`);
+      break;
+    case GameStatus.RETIRED:
+      // TODO unloads the game and maybe also navigate out from the playing page
+      console.info(`The host ended the current game.`);
+      break;
+  }
+}
+
+function handleGameListMessage(message: Message<GameListMessage>) {
+  if (isHost.value) return;
+  // TODO update UI's game list based on the host-sent message
+}
+
+function handleGameStateMessage(message: Message<GameStatusMessage>) {
+  if (message.type !== MessageType.GAME_STATE) throw "Wrong message type";
+  if (message.value?.type == null) return;
+  // TODO pass the message to the game plugin
+}
+
 export const handleMessage = (message: Message, connection: DataConnection) => {
   const handler = messageHandlerDict[message.type];
   if (!handler) {
     console.error("No message handler found for", message);
     return;
   }
+  // TODO maybe only the inner message is needed to be passed to the handler
   handler(message, connection);
 };
 
