c4e1d1332e19637b283a12e0deca8ac21448c488
Sean Wong <seanwong24@live.com>
2025-07-17 01:30:09 +0000

    feat: Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	server: {
-		allowedHosts: true
-	}
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [],
+      manifest: {
+        name: "A Game Room",
+        short_name: "Game Room",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#808080",
+        theme_color: "#808080",
+        icons: [
+          {
+            src: "/icons/icon_192.webp",
+            sizes: "192x192",
+            type: "image/webp",
+          },
+          {
+            src: "/icons/icon_512.webp",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    target: "chrome126",
+  },
+  server: {
+    allowedHosts: true,
+  },
 });
