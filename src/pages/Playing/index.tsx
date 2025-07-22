73a67adc70c02f84e528a4c649cd7a30e3a01ba9
Sean Wong <seanwong24@live.com>
2025-07-22 02:31:19 +0000

    modify game loading logic

--- src/pages/Playing/index.tsx
+++ src/pages/Playing/index.tsx
@@ -1,7 +1,7 @@
-import { useSignalEffect } from "@preact/signals";
+import { useSignal, useSignalEffect } from "@preact/signals";
 import { useLocation } from "preact-iso";
 import { isHost, peer } from "../../utils/peer";
-import { useSignalRef } from "@preact/signals/utils";
+import { Show, useSignalRef } from "@preact/signals/utils";
 import {
   currentGamePluginIframe,
   GameStateMessageType,
@@ -14,6 +14,7 @@ import "./style.css";
 export function Playing() {
   const { route } = useLocation();
   const iframeRef = useSignalRef<HTMLIFrameElement>(null);
+  const game = useSignal(""); // TODO this is temp
 
   useSignalEffect(() => {
     function handleMessage(event: MessageEvent) {
@@ -36,7 +37,10 @@ export function Playing() {
 
   useSignalEffect(() => {
     currentGamePluginIframe.value = iframeRef.current;
-    if (!iframeRef.current) console.error("Iframe is not available.");
+    if (!iframeRef.current) {
+      console.error("Iframe is not available.");
+      return;
+    }
     iframeRef.current.addEventListener("load", () => {
       sendMessageToTheGamePlugin({
         type: GameStateMessageType._PLAYER_INFO,
@@ -55,11 +59,30 @@ export function Playing() {
 
   return (
     <section class="playing page">
-      <iframe
-        class="game-plugin"
-        ref={iframeRef}
-        src="/tie-tac-toe/index.html"
-      />
+      {game.value ? (
+        <iframe class="game-plugin" ref={iframeRef} src={game.value} />
+      ) : (
+        <div>
+          <b>This is a temp selection</b>
+          <br />
+          <button
+            class="neumo"
+            onClick={() => {
+              game.value = "/games/rock-paper-scissors/index.html";
+            }}
+          >
+            Rock, Paper, and Scissors
+          </button>
+          <button
+            class="neumo"
+            onClick={() => {
+              game.value = "/games/tic-tac-toe/index.html";
+            }}
+          >
+            Tic Tac Toe
+          </button>
+        </div>
+      )}
     </section>
   );
 }
